5878ab738ac96e3d00ef3f475da621a0251343cc
Duncan McGreggor <duncan@dreamhost.com>
2012-03-30 15:14:44 -0400

    Recrafted this module to use the expected S-expression approach:  * created a new Tasks class to track config state  * rewrote the old functions in terms of s-expressions (both direct creation as    well as computed) and put them in the Tasks class  * removed all the old functions

--- dreambuilder/packager/tasks.py
+++ dreambuilder/packager/tasks.py
@@ -4,60 +4,52 @@ from twisted.internet import defer, reactor, utils
 from twisted.python import log
 
 from dreambuilder import exceptions
+from dreambuilder.command import CommandExpression as CExp
 
 
-def initialize(config):
-    if config.debug:
-        log.msg("Initializing installation ...")
-    return [
-        "sudo apt-get install -y %s" % config.pre_install_deps,
-        "mkdir -p %s" % config.install_dir]
+class Tasks(object):
+    """
+    """
+    def __init__(self, config):
+        self.config = config
+        self.pre_install_cexp = CExp(
+            CExp("sudo apt-get install -y %s" % self.config.pre_install_deps),
+            CExp("mkdir -p %s" % self.config.install_dir),
+            message="Initializing installation ...",
+            halt_on_fail=True
+        )
+        self.post_install_cexp = CExp(
+            CExp("chown -R %s %s" % (config.user, config.install_dir)),
+            message="Running final commands ..."
+        )
+
+    def build_repos_cexp(config, just_lp=False, just_git=False):
+        commands = []
+        for data in config.upstream_repos:
+            uri = data["uri"]
+            name = data["name"]
+            if config.debug:
+                log.msg("%s, %s" % (uri, name))
+            if not just_git and uri.startswith("lp"):
+                cexp = CExp(
+                    "bzr branch %s %s" % (
+                        uri, os.path.join(config.install_dir, name)),
+                    message="Installing Launchpad repo %s from %s ..." % (
+                        name, uri))
+            elif not just_lp and uri.startswith('git'):
+                cexp = CExp(
+                    "git clone %s %s" % (
+                        uri, os.path.join(config.install_dir, name)),
+                    message="Installing Git repo %s from %s ..." % (
+                        name, uri))
+            commands.append(cexp)
+        return commands
 
 
-def _install_lp_repo(uri, name, config):
-    if config.debug:
-        log.msg("Installing Launchpad repo %s from %s ..." % (name, uri))
-    return ["bzr branch %s %s" % (uri, os.path.join(config.install_dir, name))]
-
-
-def _install_git_repo(uri, name, config):
-    if config.debug:
-        log.msg("Installing Git repo %s from %s ..." % (name, uri))
-    return ["git clone %s %s" % (uri, os.path.join(config.install_dir, name))]
-
-
-def install_repos(config, just_lp=False, just_git=False):
-    commands = []
-    for data in config.upstream_repos:
-        uri = data["uri"]
-        name = data["name"]
-        if config.debug:
-            print uri, name
-        if not just_git and uri.startswith("lp"):
-            commands.extend(_install_lp_repo(uri, name, config))
-        elif not just_lp and uri.startswith('git'):
-            commands.extend(_install_git_repo(uri, name, config))
-    return commands
-
-
-def install_lp_repos(config):
-    return install_repos(config, just_lp=True)
-
-
-def install_git_repos(config):
-    return install_repos(config, just_git=True)
-
-
-def finalize(config):
-    if config.debug:
-        log.msg("Running final commands ...")
-    return ["chown -R %s %s" % (config.user, config.install_dir)]
-
-
-command_mapper = {
-    'install': {
-        'lp-repos': install_lp_repos,
-        'git-repos': install_git_repos,
-        'repos': install_repos,
-        },
-    }
+def complete_install(config):
+    tasks = Tasks(config)
+    return CExp(
+        tasks.pre_install_cexp,
+        tasks.build_repos_cexp(),
+        tasks.post_install_cexp
+    )
