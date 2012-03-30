0d3350a75098433529ee4b099ac91c368db1387d
Duncan McGreggor <duncan@dreamhost.com>
2012-03-30 16:42:20 -0400

    Small refactoring to clean up __init__.

--- dreambuilder/command.py
+++ dreambuilder/command.py
@@ -17,16 +17,25 @@ class CommandExpression(SExpression):
         self.message = kwargs.get("message") or ""
         self.skip = kwargs.get("skip") or False
         self.children = []
+        self.check_args(args)
+        self.check_children()
+
+    def check_args(self, args):
         nested_index = 1
-        if args:
-            first_arg = args[0]
-            if first_arg and isinstance(first_arg, basestring):
-                if self.command:
-                    raise exceptions.MultipleCommandsError()
-                self.command = first_arg
-            elif isinstance(first_arg, CommandExpression):
-                nested_index = 0
-            self.children = args[nested_index:]
+        if not args:
+            return
+        first_arg = args[0]
+        if first_arg and isinstance(first_arg, basestring):
+            if self.command:
+                raise exceptions.MultipleCommandsError()
+            self.command = first_arg
+        elif isinstance(first_arg, CommandExpression):
+            nested_index = 0
+        # now that we know where the nested commands start, we can set the
+        # children
+        self.children = args[nested_index:]
+
+    def check_children(self):
         for child in self.children:
             if isinstance(child, basestring):
                 raise exceptions.MultipleCommandsError()
