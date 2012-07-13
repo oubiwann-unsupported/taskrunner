d7a4bf0e387c518d2cb53048d7a4a915ee6254f0
Duncan McGreggor <duncan@dreamhost.com>
2012-07-13 15:23:47 -0400

    Removed unused plugin.

--- twisted/plugins/dreambuilder_archiver.py
+++
@@ -1,8 +0,0 @@
-from twisted.application.service import ServiceMaker
-
-
-name = "archiver"
-module = "dreambuilder.archiver.service"
-description = "DreamHost's Archive-building Tools"
-tapname = name
-serviceMaker = ServiceMaker(name, module, description, tapname)
